bf917614834e78b8621865e88902c5da0a6c0d8f
Hroman9n <s.vasin99@mail.ru>
2021-03-10 17:54:44 +0300

    reimagined main function

--- 2list_comp.py
+++ 2list_comp.py
@@ -1,22 +1,38 @@
+### костыль, который я написал перед там как вспомнил,
+###  что можно сравнивать множества
+
+# def comp(list1, list2):
+#     setl1 = list(set(list1))
+#     setl2 = list(set(list2))
+
+#     if len(setl1) != len(setl2):                                # если длинна списков не совпадает то множества уже не равны
+#         print("\nМножества из данных списков не совпадают\n")   # поэтому нам не нужно сравнивать какое из множеств больше чтобы взять за основу первого цикла
+#         return
+#     else:
+#         # идем по каждому предмету в первом списке, 
+#         # если находим его во втором то удаляем его
+#         for i in setl1:
+#             for j in range(len(setl2)):
+#                 if setl2[j] == i:
+#                     setl2.remove(setl2[j])
+#                     break
+    
+#     if setl2 != []:
+#         print("\n Множества из данных списков не совпадают\n")
+#     else:
+#         print("\nМножества из данных списков совпадают\n")
+
+
 
 def comp(list1, list2):
-    setl1 = list(set(list1))
-    setl2 = list(set(list2))
 
-    if len(setl1) != len(setl2):
-        print("\nМножества из данных списков не совпадают\n")
-        return
-    else:
-        for i in setl1:
-            for j in range(len(setl2)):
-                if setl2[j] == i:
-                    setl2.remove(setl2[j])
-                    break
-    
-    if setl2 != []:
-        print("\n Множества из данных списков не совпадают\n")
-    else:
+    a = set(list1)
+    b = set(list2)
+
+    if a == b:
         print("\nМножества из данных списков совпадают\n")
+    else:
+        print("\n Множества из данных списков не совпадают\n")
 
 
 # тестовые списки #
@@ -40,10 +56,17 @@ while 1:
         exit()
     elif ask == "2":
         comp(frst, scnd)
-    # elif ask == "1":
-    #     # frsti = input("\nВведите первый список: ")
-    #     # scndi = input("Введите второй список: ")
+    elif ask == "1":
+        try:
+            frsti = list(input("Введите значения первого списка через пробел: ").split())
+            frsti = list(map(int, frsti))
 
+            scndi = list(input("Введите значения первого списка через пробел: ").split())
+            scndi = list(map(int, scndi))
+        except ValueError:
+            print("Ошибка ввода. Попробуйте ещё раз")
+
+        comp(frsti, scndi)
 
     else:
         print("\nОшибка ввода. Попробуйте ещё раз")
